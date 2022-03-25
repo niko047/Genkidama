e4d43522ad03ce8e4117c8795213a93f83a60143
Miyamoto Musashi <miyamoto@Miyamotos-Air.fritz.box>
2022-03-25 12:07:10 -0300

    Agent can sample from self or shared buffer

--- replay_buffer/buffer.py
+++ replay_buffer/buffer.py
@@ -1,15 +1,17 @@
 import torch
 import torch.multiprocessing as mp
 
+
 class ReplayBuffers(object):
 
-    def __init__(self, shared_replay_buffer, cpu_id, max_num_interactions, batch_size):
+    def __init__(self, shared_replay_buffer, cpu_id, len_interaction, num_iters, batch_size, tot_num_cpus, replacement):
         self.shared_replay_buffer = shared_replay_buffer
         self.cpu_id = cpu_id
         self.current_iter_number = 0
-        self.max_num_interactions = max_num_interactions
-
-        self.has_been_filled = False
+        self.len_interaction = len_interaction
+        self.num_iters = num_iters
+        self.tot_num_cpus = tot_num_cpus
+        self.replacement = replacement
         self.batch_size = batch_size
 
     @staticmethod
@@ -36,16 +38,13 @@ class ReplayBuffers(object):
             # Uses a First-In First-Out way of rewriting the buffer by resetting index
             self.current_iter_number = 0
 
-            # Setting it to true, it is going to help indexing while random sampling
-            self.has_been_filled = True
-
         self.shared_replay_buffer[self.cpu_id, self.current_iter_number, :] = new_inter
 
         self.current_iter_number += 1
 
     def is_buffer_full(self):
         """Checks if the buffer for current CPU is full"""
-        return True if (self.max_num_interactions - self.current_iter_number + 1) == 0 else False
+        return True if (self.len_interaction - self.current_iter_number + 1) == 0 else False
 
     @staticmethod
     def random_sample_batch_(shared_buffer: torch.Tensor,
@@ -53,10 +52,16 @@ class ReplayBuffers(object):
                              num_iters: int,
                              tot_num_cpus: int,
                              batch_size: int,
-                             replacement=False) -> torch.Tensor:
+                             replacement: bool = False,
+                             cpu_id=None) -> torch.Tensor:
         """Returns a random sample over the shared CPU buffers, mixing them up"""
-        reshaped_shared_buffer = torch.reshape(shared_buffer, (num_iters*tot_num_cpus, len_interaction))
-        
+        if cpu_id is None:
+            reshaped_shared_buffer = torch.reshape(shared_buffer, (num_iters * tot_num_cpus, len_interaction))
+        elif isinstance(cpu_id, int):
+            reshaped_shared_buffer = shared_buffer[cpu_id]
+        else:
+            raise Exception('cpu_id does not conform to type None or int')
+
         # Creates boolean mask to exclude rows that are still zero
         zero_row = torch.zeros(size=(len_interaction,))
         mask = ~(reshaped_shared_buffer == zero_row)[:, 0]
@@ -73,16 +78,13 @@ class ReplayBuffers(object):
 
         return masked_buffer[idxs]
 
-    def random_sample_batch(self, replacement=False):
+    def random_sample_batch(self, from_shared_memory):
         """Random samples a batch from shared buffer to update the network's weights"""
-        max_idx = len(self.shared_replay_buffer[0]) if self.has_been_filled else self.current_iter_number + 1
-
-        if replacement:
-            # Random sample without fear of full of zeros-rows
-            idxs = torch.randint(max_idx, (self.batch_size,))
-        else:
-            idxs = torch.randperm(max_idx)[:self.batch_size]
-
-        return self.shared_replay_buffer[self.cpu_id][idxs]
-
-
+        update_batch = self.random_sample_batch_(shared_buffer=self.shared_replay_buffer,
+                                                 len_interaction=self.len_interaction,
+                                                 num_iters=self.num_iters,
+                                                 tot_num_cpus=self.tot_num_cpus,
+                                                 batch_size=self.batch_size,
+                                                 replacement=self.replacement,
+                                                 cpu_id=None if from_shared_memory else self.cpu_id)
+        return update_batch
--- replay_buffer/main.py
+++ replay_buffer/main.py
@@ -1,4 +1,4 @@
-from replay_buffer.replay_buffer import ReplayBuffers
+from buffer import ReplayBuffers
 import torch
 import torch.multiprocessing as mp
 
@@ -8,12 +8,38 @@ LEN_SINGLE_STATE = 4
 LEN_ITERATIONS = 5
 NUM_CPUS = mp.cpu_count()
 
+class Manager(object):
+    def __init__(self):
+        self.a = 5
+
+
 def foo(t, i, max_num_interactions):
-    b = ReplayBuffers(shared_replay_buffer=t, cpu_id=i, max_num_interactions=max_num_interactions, batch_size=3)
+    b = ReplayBuffers(shared_replay_buffer=t, cpu_id=i, len_interaction=LEN_SINGLE_STATE, batch_size=3, num_iters=LEN_ITERATIONS,
+                      tot_num_cpus = NUM_CPUS, replacement = False)
 
     # Get the array from shared memory and reshape it into a numpy array
     for j in range(LEN_ITERATIONS):
         b.record_interaction(torch.Tensor([i]*4))
+
+    print(f'Random sampled batch is {b.random_sample_batch(from_shared_memory=False)}')
+
+def task_handler(cpu_id):
+    if cpu_id == 0:
+        # Behave as the central node handling updates
+        pass
+    else:
+        # Behave as a working node
+        pass
+
+# TODO - Define a class Manager
+# TODO - Define a Maneger.task_handler
+# TODO - Define a Manager.semaphor
+
+# TODO - Implement a manager class, it should start random sampling once all CPUs have reached a certain number
+# TODO - of steps in their environment. Then they can all go freely. After that, Implement the result of the random sampling
+# TODO - in such a way that the random sampled tensors go inside a multiprocessing queue and get ready for updating the network
+# TODO - Every X interactions with the environment, the workers check if the queue is busy, and if so take one bacth and
+# TODO - perform one update to the global parameters of the network.
 
 
 if __name__ == '__main__':
@@ -34,6 +60,5 @@ if __name__ == '__main__':
                              tot_num_cpus = NUM_CPUS,
                              batch_size = 3,
                              replacement=False)
-    print(f'Random sampled array is {sample}, memory shared {sample.is_shared()}')
 
     print(f'buffer is {buffer} shared memory {buffer.is_shared()}')
