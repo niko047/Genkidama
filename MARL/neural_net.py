a386d464640eaf81ecb3dca75b7d853e8536ed1e
Miyamoto Musashi <miyamoto@Miyamotos-Air.fritz.box>
2022-03-29 22:39:38 -0300

    ToyNet class

--- MARL/neural_net.py
+++ MARL/neural_net.py
@@ -2,13 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def initialize(layers):
-    for layer in layers:
-        nn.init.normal_(layer.weight, mean=0, std=0.1)
-        nn.init.constant_(layer.bias, .0)
-
-
-class NNet(nn.Module):
+class ToyNet(nn.Module):
 
     def __init__(self):
         super().__init__()
@@ -16,10 +10,17 @@ class NNet(nn.Module):
         self.fc2 = nn.Linear(in_features=32, out_features=32)
         self.fc3 = nn.Linear(in_features=32, out_features=1)
 
-        initialize([self.fc1, self.fc2, self.fc3])
+        ToyNet.initialize_layers([self.fc1, self.fc2, self.fc3])
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         return x
+
+    @staticmethod
+    def initialize_layers(layers):
+        for layer in layers:
+            nn.init.normal_(layer.weight, mean=0, std=0.1)
+            nn.init.constant_(layer.bias, .0)
+
